0858f2030ec296ea8549724b5c3ea67c9ac19b00
Max Stein <maxwell.stein@gmail.com>
2023-06-13 21:16:56 -0700

    updates pause button size

--- src/components/PauseButton.vue
+++ src/components/PauseButton.vue
@@ -1,6 +1,6 @@
 <template>
-  <svg width="100%" height="100%" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect width="4" height="12" rx="0.75" fill="black" />
-    <rect x="8" width="4" height="12" rx="0.75" fill="black" />
+  <svg width="11" height="14" viewBox="0 0 11 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="3.66667" height="14" rx="0.75" fill="black" />
+    <rect x="7.33334" width="3.66667" height="14" rx="0.75" fill="black" />
   </svg>
 </template>
